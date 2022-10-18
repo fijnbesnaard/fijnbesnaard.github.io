857d056777cb4f765a5dab9eecc481649e08c269
Niels Hartog <niels@fijnbesnaard.nl>
2022-10-18 15:49:21 +0200

    gradient bg + axios request to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,61 @@
-import React from "react";
-import '../scss/App.scss';
+import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import '../scss/App.scss';
 
 class App extends React.Component {
 
   state = {
-    errorMessage: ''
+    errorMessage: '',
+    searchFor: ''
   };
 
   componentDidMount() {
     // setState();
   }
 
+  onSearchSubmit = (term) => {
+    if (term) {
+      console.log(term);
+
+      axios.get('https://api.unsplash.com/search/photos', {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization: 'Client-ID XyXCXWVZBa4404exjAQxrWVxoGgS1Bc9UwqrB8D9HN4',
+        }
+      }).then((response) => {
+        console.log(response);
+        console.log(response.data.results);
+      });
+
+    } else {
+      console.log('do nothing!');
+    }
+  }
+
   renderContent() {
     // conditionally return jsx
+  }
+
+  resultsList() {
+    if (this.state.searchFor) {
+      return (
+        <h1>Searching for {this.state.searchFor}...</h1>
+      )
+    };
+    return;
   }
 
   render() {
     return (
 
       <div className="container">
-        <SearchBar action="Zoek" placeholder="Zoek..." />
-        <SearchBar />
-        {/* <ButtonAnimated buttonText='Search' iconName='search' /> */}
+        <SearchBar action="Zoek" placeholder="Zoek..." onSubmit={this.onSearchSubmit} />
+        {/* <SearchBar /> */}
         {/* <ResultsList */}
+        {this.resultsList()}
       </div>
     )
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { Input } from 'semantic-ui-react'
+import { Input, Button, Icon } from 'semantic-ui-react'
+// import ButtonAnimated from './ButtonAnimated';
 import '../scss/SearchBar.scss';
 
 class SearchBar extends React.Component {
 
+  state = {
+    searchTerm: '',
+    searchFor: ''
+  }
+  // // Register changes on input
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(this.state.searchTerm);
+    this.props.onSubmit(this.state.searchTerm);
+  }
+
   render() {
 
     return (
-      <Input className='input-action' action={this.props.action} placeholder={this.props.placeholder} />
+      <div className="search-bar">
+        <h1>Zoek afbeeldingen</h1>
+        <form className="form" onSubmit={this.onSubmitForm}>
+          <Input className='input-action' placeholder={this.props.placeholder}>
+            <input type="text" value={this.state.searchTerm} onChange={e => this.setState({ searchTerm: e.target.value })} />
+          </Input>
+          <Button animated>
+            <Button.Content visible>Zoek</Button.Content>
+            <Button.Content hidden>
+              <Icon name="search" />
+            </Button.Content>
+          </Button>
+          {/* <ButtonAnimated buttonText='Zoek' iconName="search" onClick={() => this.setState({ searchFor: this.state.searchTerm })} /> */}
+        </form>
+        {this.state.searchFor}
+      </div>
     );
   }
 }
